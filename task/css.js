bce18dd1e42b287a3ca2b947d930e3f872227282
이희원 <richready2011@gmail.com>
2024-06-02 13:21:28 +0900

    리팩토링

--- task/css.js
+++ task/css.js
@@ -1,11 +1,9 @@
 import sass from 'sass'
-
-import fs from 'fs'
+import fs from 'fs/promises'
 import globby from 'globby'
 import postcss from 'postcss'
 import autoprefixer from 'autoprefixer'
-import postcssReporter from "postcss-reporter";
-
+import postcssReporter from "postcss-reporter"
 import { configs, plugins } from '../configs'
 
 let argv = process.argv.slice( 2 )
@@ -19,52 +17,30 @@ function compatiblePath( str ) {
   return str.replace( /\\/g, '/' )
 }
 
-function workPostCss( css, pathOut, fileName, prevMap ) {
-  return postcss( [
+async function workPostCss( css, pathOut, fileName, prevMap ) {
+  let result = await postcss( [
     autoprefixer,
-    postcssReporter({ clearReportedMessages: true })
-  ] )
-    .process( css, {
-      from: pathOut,
-      map: prevMap ? { prev: prevMap, inline: false, annotation: false } : { inline: false, annotation: false }
-    } )
-    .then( result => {
-
-      writeFile( pathOut, fileName, result.css );
-
-      if ( result.map && configs.sourceMap ) {
-        // SourceMap을 별도의 파일로 저장합니다.
-        writeFile( pathOut, fileName + '.map', result.map.toString() );
-      }
-
-    } );
-}
-
-function writeFile( pathOut, fileName, fileData = true ) {
-
-  fs.mkdir( pathOut, { recursive: true }, function ( err ) {
-
-    if ( err ) throw err;
-
-    fs.writeFile( `${ pathOut }${ fileName }`, fileData, function ( err ) {
-      if ( err ) throw err;
-
-      const data = fs.readFileSync( `${ pathOut }${ fileName }` );
-      const fd = fs.openSync( `${ pathOut }${ fileName }`, 'w+' );
-
-      fs.writeSync( fd, data, 0, data.length, 0 );
-      fs.close( fd, function ( err ) {
-        if ( err ) throw err;
-        console.log( `[Scss 컴파일] ${ pathOut }${ fileName }` );
-      } )
-    } )
+    postcssReporter( { clearReportedMessages: true } )
+  ] ).process( css, {
+    from: pathOut,
+    map: prevMap ? { prev: prevMap, inline: false, annotation: false } : { inline: false, annotation: false }
   } )
+
+  await writeFile( pathOut, fileName, result.css )
+
+  if ( result.map && configs.sourceMap ) {
+    await writeFile( pathOut, fileName + '.map', result.map.toString() )
+  }
 }
 
-function parseSass( srcFiles ) {
+async function writeFile( pathOut, fileName, fileData = true ) {
+  await fs.mkdir( pathOut, { recursive: true } )
+  await fs.writeFile( `${ pathOut }${ fileName }`, fileData )
+  console.log( `[Scss 컴파일] ${ pathOut }${ fileName }` )
+}
 
-  srcFiles.forEach( srcFile => {
-
+async function parseSass( srcFiles ) {
+  let promises = srcFiles.map( async srcFile => {
     const outFile = compatiblePath( srcFile )
       .replace( /^src/g, configs.dest )
       .replace( /\/scss\//g, '/css/' )
@@ -73,21 +49,21 @@ function parseSass( srcFiles ) {
     const outFileName = outFile.match( /[^/]+$/ )[0]
     const outFilePath = outFile.match( /^(.*\/)[^/]+$/ )[1]
 
-    sass.render( {
-      file: srcFile,
-      outFile: outFile,
-      importer: plugins.scss.importer,
-      ...configs.css
-    }, function ( err, result ) {
+    try {
+      let result = await sass.renderSync( {
+        file: srcFile,
+        outFile: outFile,
+        importer: plugins.scss.importer,
+        ...configs.css
+      } )
 
-      if ( err ) throw err.message
-
-      workPostCss( result.css.toString(), outFilePath, outFileName, (result.map) && result.map.toString() )
-
-    } )
-
+      await workPostCss( result.css.toString(), outFilePath, outFileName, (result.map) && result.map.toString() )
+    } catch ( err ) {
+      console.error( err.message )
+    }
   } )
 
+  await Promise.all( promises )
 }
 
 if ( isWatch ) {
@@ -100,15 +76,13 @@ if ( isWatch ) {
   // 파일명이 '_' 인 경우
   if ( /_[^\/]*?\.*$/.test( files[0] ) ) {
     files = configs.css.chunk.map( file => `${ configs.root }${ file }` )
-    console.log( `>> '_' 파일은 import 대상으로, 컴파일 제외, chunk 컴파일\n${files}` )
+    console.log( `>> '_' 파일은 import 대상으로, 컴파일 제외, chunk 컴파일\n${ files }` )
   }
 
   parseSass( files )
 
 } else {
-
-  globby( `${ configs.css.src }/**/!(_*).scss`, {} ).then( files => {
+  globby( `${ configs.css.src }/**/!(_*).scss` ).then( files => {
     parseSass( files )
   } )
-
 }
